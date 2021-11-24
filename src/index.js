2d8bd26296ec26bfff718d7cee34d79a87991bc7
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2021-11-24 11:53:01 -0400

    fddg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Variables y Constantes
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const nombre = "André";
+const apellido = "Pichardo";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let valorDado = 5;
+valorDado = 3;
+
+console.log(nombre, apellido, valorDado);
