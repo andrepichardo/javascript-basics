7b99e1d89a23073956cf72fc507216190815d3f3
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2021-11-24 14:58:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,5 +6,9 @@ const apellido = "Pichardo Goico";
 let valorDado = 5;
 valorDado = 7;
 
+<<<<<<< HEAD
 document.write('hola a todos los amigos')
+=======
+document.write('hola a todos')
+>>>>>>> parent of 7300f55 (env)
 console.log(nombre, apellido, valorDado);
