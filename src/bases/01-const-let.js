3dddc567afe2fb01c567a233a7fdbd6c2469e71a
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2021-11-24 15:52:51 -0400

    ok

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+// Variables y Constantes
+
+const nombre = "André";
+const apellido = "Pichardo Goico";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var NO se debe de usar...
+if (true) {
+    const nombre = 'Peter';
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-// Variables y Constantes
-
-const nombre = "André";
-const apellido = "Pichardo Goico";
-
-let valorDado = 5;
-valorDado = 4;
-
-console.log(nombre, apellido, valorDado);
+console.log('hola')
