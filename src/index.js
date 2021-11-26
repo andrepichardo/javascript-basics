3a7aea517c40da6fc74fce3b29305285092217b7
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2021-11-26 15:37:43 -0400

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// // Funciones en JS
+// const saludar = function(nombre) {
+//     return `Hola, ${nombre}`;
+// }
+
+const saludar2 = (nombre) => {
+    return `Hola, ${nombre}`;
+}
+
+const saludar3 = (nombre) => `Hola, ${nombre}`;
+const saludar4 = (nombre) => `Hola mundo`;
+
+
+// console.log(saludar('Goku'));
+
+console.log(saludar2('Vegeta'));
+console.log(saludar3('Goku'));
+console.log(saludar4());
