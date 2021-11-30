bd86d9cc466ec2148b322fa4117a809bfa6ab429
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2021-11-30 15:28:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const saludar2 = (nombre) => {
 }
 
 const saludar3 = (nombre) => `Hola, ${nombre}`;
-const saludar4 = (nombre) => `Hola mundo`;
+const saludar4 = () => `Hola mundo`;
 
 
 // console.log(saludar('Goku'));
