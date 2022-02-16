f79a4997e4c37da2be16ca62eac1b9f04e59ab98
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-02-16 12:48:27 -0400

    deses-obj

--- src/bases/06-deses-obj.js
+++ src/bases/06-deses-obj.js
@@ -20,13 +20,16 @@ const usecontext = ({ nombre, edad, clave, rango = "Capitán" }) => {
     anios: edad,
     latlng: {
       lat: 14.1232,
-      lng: -12.2323
-    }
-  }
+      lng: -12.2323,
+    },
+  };
+};
 
-}
+const {
+  nombreClave,
+  anios,
+  latlng: { lat, lng },
+} = usecontext(persona);
 
-const {nombreClave, anios, latlng:{lat,lng}} = usecontext(persona);
-
-console.log(nombreClave,anios);
-console.log(lat,lng);
+console.log(nombreClave, anios);
+console.log(lat, lng);
