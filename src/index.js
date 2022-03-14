ca5684608ab8520543e27f3a24c12fc418a9104f
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-03-14 14:49:03 -0400

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// const getImagenPromesa = () => new Promise(resolve => resolve("https://webs.com"));
+// getImagenPromesa().then(console.log);
+
+const getImage = () => {
+    return 'https://webs.com'
+}
